2c492902f2673e8b22ed2f21adc7f04bab1d1a05
xiaobo <xiaoboemail@foxmail.com>
2018-12-11 15:12:55 +0800

    图片修改

--- adidas/js/function.js
+++ adidas/js/function.js
@@ -81,7 +81,7 @@ function getTabNav(url,getEl){
                         <div class="common-tab">
                                 <p><a><i class="iconfont">&#xe618;</i>${item.tui}</a></p>
                                 <ul>
-                                    <li><a href=""><img src="../images/${item.img}"></a></li>
+                                    <li><a href=""><img src="images/${item.img}"></a></li>
                                     ${(function(){
                                         var Str = "";
                                         item.pro.forEach(function(list){
